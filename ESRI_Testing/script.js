d7bb0ef60633b3e973df0ff7e9fcabbde3949a1b
bclayman716 <benjamin.clayman@gmail.com>
2017-04-03 14:17:50 -0400

    start timer testing
    created timer testing

--- ESRI_Testing/script.js
+++ ESRI_Testing/script.js
@@ -1,14 +1,15 @@
 var map;
 var view;
 var template;
-// var popup;
+
 require(["esri/Map",
   "esri/views/MapView",
   "esri/layers/FeatureLayer",
+  "esri/widgets/Search",
 
-  // "esri/widgets/Popup",
-  // "esri/PopupTemplate",
-  "dojo/domReady!"], function(Map, MapView, FeatureLayer){
+  "dojo/on",
+  "dojo/dom",
+  "dojo/domReady!"], function(Map, MapView, FeatureLayer, Search, on, dom){
   map = new Map({
     basemap: "streets" ,
     });
@@ -20,69 +21,69 @@ require(["esri/Map",
     center: [-71.116734, 42.377061]
     });
 
-
   template = {
-    title: "Test",
-    content: "{Street_Name}"
-  }
+    title: "Street Name: {Street_Name}",
+    content:  [{
+      type: "fields",
+      fieldInfos:[{
+        fieldName: "CurrentPlan",
+        label: "Year"},
+        {
+        fieldName: "Webpage",
+        label: "Webpage"
+        }]
+      }]
+    }
+
 
   future_projs = new FeatureLayer({
      url: "https://gis.cambridgema.gov/arcgis/rest/services/DPWEmbeddedLayers/MapServer/5",
     outFields: ["*"],
-     popupTemplate: template
+    popupTemplate: template
    });
    map.add(future_projs);
-  // current_projs = new FeatureLayer({
-  //     url: "https://gis.cambridgema.gov/arcgis/rest/services/DPWEmbeddedLayers/MapServer/6"
-  //   });
-  //  map.add(current_projs);
-  // past_projs = new FeatureLayer({
-  //      url: "https://gis.cambridgema.gov/arcgis/rest/services/DPWEmbeddedLayers/MapServer/7"
-  //    });
-  //  map.add(past_projs);
+  current_projs = new FeatureLayer({
+      url: "https://gis.cambridgema.gov/arcgis/rest/services/DPWEmbeddedLayers/MapServer/6",
+      outFields: ["*"],
+      popupTemplate: template
+    });
+   map.add(current_projs);
+  past_projs = new FeatureLayer({
+       url: "https://gis.cambridgema.gov/arcgis/rest/services/DPWEmbeddedLayers/MapServer/7",
+       outFields: ["*"],
+       popupTemplate: template
+     });
+   map.add(past_projs);
 
 
+  var searchWid = new Search({
+    view: view
+  });
 
 
+  //Add search widget to top left corner
+  view.ui.add(searchWid,{
+    position: "top-left",
+    index: 0
+  });
+
+  //  Change basemap
+    on(dom.byId("btnStreets"),"click", function() {
+      map.basemap = "streets";
+    });
+    on(dom.byId("btnSatellite"),"click", function() {
+      map.basemap ="satellite";
+    });
+    on(dom.byId("btnHybrid"),"click", function() {
+     map.basemap ="hybrid";
+    });
+    on(dom.byId("btnTopo"),"click", function() {
+     map.basemap ="topo";
+    });
+    on(dom.byId("btnGray"),"click", function() {
+      map.basemap ="gray";
+    });
+    on(dom.byId("btnNatGeo"),"click", function() {
+      map.basemap = "national-geographic";
+    });
 });
-
-
-// var map;
-// var view;
-// require(["esri/Map",
-//       "esri/views/MapView",
-//       "dojo/on",
-//       "dojo/dom",
-//       "dojo/domReady!"], function(Map, MapView, on, dom){
-//     // Create map
-//     map = new Map({
-//       basemap: "streets",
-//     });
-//     view = new MapView({
-//       container: "viewDiv",
-//       map: map,
-//       zoom: 14,
-//       // center: [-71.349444, 42.460278] // Concord Center
-//       center: [-71.116734, 42.377061]
-//       });
-//
-//     // Wire UI Events
-//     on(dom.byId("btnStreets"),"click", function() {
-//       map.setBasemap("streets");
-//     });
-//     on(dom.byId("btnSatellite"),"click", function() {
-//       map.setBasemap("satellite");
-//     });
-//     on(dom.byId("btnHybrid"),"click", function() {
-//       map.setBasemap("hybrid");
-//     });
-//     on(dom.byId("btnTopo"),"click", function() {
-//       map.setBasemap("topo");
-//     });
-//     on(dom.byId("btnGray"),"click", function() {
-//       map.setBasemap("gray");
-//     });
-//     on(dom.byId("btnNatGeo"),"click", function() {
-//       map.setBasemap("national-geographic");
-//     });
-// });
